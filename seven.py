930f8a0f3848e88481bf981448df74ec75b7e038
boniato <teegeegle@gmail.com>
2018-06-23 21:47:37 +0900

    7.약수의 합(sum of divisor)
    자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
    제한 사항: n은 0 이상 3000이하인 자연수입니다.
    입출력 예: 12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.

--- seven.py
+++ seven.py
@@ -1,13 +1,3 @@
-#자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
-
-#제한 사항
-#n은 0 이상 3000이하인 자연수입니다.
-
-#입출력 예 #1
-#12의 약수는 1, 2, 3, 4, 6, 12입니다. 이를 모두 더하면 28입니다.
-
-
-
 def solution(n):
     answer = 0
     
@@ -18,9 +8,6 @@ def solution(n):
                 answer += i
             
     return answer
-
-solution(12)
-
 
 
 ### The below is other person's code example ###
