4bc0ff11a331f4ca04849c431423dbe9ebd50b0c
boniato <teegeegle@gmail.com>
2018-07-03 22:34:21 +0900

    피보나치 수(Fibonacci numbers)
    피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 점화식입니다. 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
    
    입출력 예
    n	return
    3	2
    5	5
    
    입출력 예 설명
    피보나치수는 0번째부터 0, 1, 1, 2, 3, 5, ... 와 같이 이어집니다.
    
    cf.) 피보나치 수열: (1, 1, 2, 3, 5, 8, 13 …처럼 1로 시작하고, 앞 두 수의 합이 그 다음 수로 이뤄진 수열)

---
+++ twelve.java
@@ -0,0 +1,33 @@
+public class Solution {
+	
+  public int solution1(int n) {
+	    int result;
+	    int a = 0;
+	    int b = 1;
+	         
+      if(n==0) return 0;
+      else if(n==1) return 1;
+      else {
+        for(int i=1; i<n; i++) {
+          result = a + b;
+          a = b;
+          b = result;
+        }
+      }
+      
+      return b;
+	  }
+    
+    public int solution2(int n) {
+	     int answer = 0;
+	     
+	     if(n<=1) {
+	    	 return n;
+	     }else{
+	    	 answer = solution(n-2) + solution(n-1);
+	     }
+	     
+	     return answer;
+	  }
+    
+}
