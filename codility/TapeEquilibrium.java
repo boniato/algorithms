014aaf4a9632e6ae837b1790394199df1bc06e93
boniato <teegeegle@gmail.com>
2018-08-14 00:16:36 +0900

    Lesson3-3 (Time Complexity)
    A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
    Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
    The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
    In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
    For example, consider array A such that:
    
      A[0] = 3
      A[1] = 1
      A[2] = 2
      A[3] = 4
      A[4] = 3
    We can split this tape in four places:
    
    P = 1, difference = |3 − 10| = 7 
    P = 2, difference = |4 − 9| = 5 
    P = 3, difference = |6 − 7| = 1 
    P = 4, difference = |10 − 3| = 7

--- codility/TapeEquilibrium.java
+++ codility/TapeEquilibrium.java
@@ -1,21 +1,23 @@
-public static int TapeEquilibrium(int[] A) {
-	    int right = 0;
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int right = 0;
 	    int left = 0;
 	    int sum = 0;
-	    int min = 100001;
+	    int min = 100001; //N is an integer within the range [2..100,000]
 	    
-	    for (int it : A)
-	        right += it;
-	    	//System.out.println(right);
+	    for (int element : A)
+	        right += element; //sum of array A
 	        
 	    for (int p=1; p < A.length; ++p) {
-	        right -= A[p - 1]; //10, 9, 7, 3
-	        left += A[p - 1];  //3, 4, 6, 10
+	        right -= A[p-1]; //10, 9, 7, 3
+	        left += A[p-1];  //3, 4, 6, 10
 	        sum = Math.abs(right - left); //7, 5, 1, 7
 	        
-	        if (min > sum) // find the smallest number
+	        if (min > sum) //Find the smallest number
 	            min = sum;
 	    }
 	    
-	    return min; 
-	}
+	    return min;
+    }
+}
