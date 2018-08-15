a1c8c98b42efde066813e3353fdaaa851a1211b2
boniato <teegeegle@gmail.com>
2018-08-15 23:02:07 +0900

    Lesson4-1 (Counting Elements)
    Given an array A, returns 1 if array A is a permutation and 0 if it is not.
    
    Assume that:
    N is an integer within the range [1..100,000];
    each element of array A is an integer within the range [1..1,000,000,000].
    
    Complexity:
    expected worst-case time complexity is O(N);
    expected worst-case space complexity is O(N) (not counting the storage required for input arguments).

--- codility/PermCheck.java
+++ codility/PermCheck.java
@@ -6,21 +6,21 @@ class PermCheck {
         int min = 1000000000;
         int max = 0;
         int res = 1;
-        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<Integer>(); //Set is no duplicate data and no order
         
         for(int i : A){
-        	if (set.contains(i)) {
+        	if (set.contains(i)) { //check duplicate data
         		 res = 0;
                  break;
         	}
         	
-        	set.add(i);
+            set.add(i);
             min = Math.min(min, i);
             max = Math.max(max, i);
         }
         
         if(min != 1) res = 0;
-        if(max != A.length) res = 0;
+        if(max != A.length) res = 0; //If array A is a permutation, it is same with max value.
         
         return res;
     }
