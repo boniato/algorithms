a4843da4e8fbf54abe22e0ac7f3b971247a44d25
boniato <teegeegle@gmail.com>
2018-08-21 23:26:00 +0900

    Lesson4-4 (Counting Elements), 100%
    Given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
    For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
    Given A = [1, 2, 3, 4], the function should return 5.
    Given A = [−1, −3], the function should return 1.
    
    expected worst-case time complexity is O(N);
    expected worst-case space complexity is O(N)

--- codility/MissingInteger.java
+++ codility/MissingInteger.java
@@ -4,16 +4,16 @@ import java.util.HashSet;
 class MissingInteger {
     public int solution(int[] A) {
        int smallestVal = 1;
-        HashSet<Integer> set = new HashSet<>();
+       HashSet<Integer> set = new HashSet<>();
     
-        for(int i=0; i<A.length; ++i){
+       for(int i=0; i<A.length; ++i){
             if(A[i]<1) continue;
             if(set.add(A[i])){
                 while(set.contains(smallestVal)){
                     smallestVal++;
                 }
             }
-        }
+       }
     
         return smallestVal;
     }
