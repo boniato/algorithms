e5827bcfd3ce5b92222609170ec63d259a96a057
boniato <teegeegle@gmail.com>
2018-08-25 01:10:12 +0900

    Lesson6-2 (Sorting) 100%
    A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
    For example, array A such that:
      A[0] = -3
      A[1] = 1
      A[2] = 2
      A[3] = -2
      A[4] = 5
      A[5] = 6
    contains the following example triplets:
    (2, 4, 5), product is 2 * 5 * 6 = 60
    Your goal is to find the maximal product of any triplet.
    Given a non-empty array A, returns the value of the maximal product of any triplet.
    the function should return 60, as the product of triplet (2, 4, 5) is maximal.
    
    Assume that:
    N is an integer within the range [3..100,000];
    each element of array A is an integer within the range [−1,000..1,000].
    
    Complexity:
    expected worst-case time complexity is O(N*log(N));
    expected worst-case space complexity is O(1) (not counting the storage required for input arguments).

--- codility/MaxProductOfThree.java
+++ codility/MaxProductOfThree.java
@@ -1,7 +1,26 @@
 import java.util.Arrays;
 
 class MaxProductOfThree {
-    public int solution(int[] A) {
+    
+    /* good code (score:100%)
+     * If A[0] and A[1] are negative numbers, the product of two numbers will be positive numbers,
+     * and multiply it by A[N-1]. The below is the example.
+     * {-7, -5, -3, -1, 2}; //6, 70
+     * {-5, -3, -1, 2, 7};  //-14, 105
+     */
+    public static int solution1(int[] A) {
+        int N = A.length;
+        Arrays.sort(A);
+         
+        int product1 = A[N-3] * A[N-2] * A[N-1];
+        int product2 = A[0] * A[1] * A[N-1];
+     
+        return product1 > product2 ? product1 : product2;
+    }
+    
+    
+    //not good code
+    public int solution2(int[] A) {
         Arrays.sort(A);
         int triplet = 3;
         int maxProduct = 1;
