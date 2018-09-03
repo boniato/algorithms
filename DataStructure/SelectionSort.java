8f9403de674ac7af559b5f5f2058f50bf4320c3f
boniato <teegeegle@gmail.com>
2018-09-03 22:30:15 +0900

    삽입 정렬
    의사코드 : 
    for i = 0 to n:
    a[i]부터 a[n - 1]까지 차례로 비교하여 가장 작은 값이 a[j]에 있다고 하자.
    a[i]와 a[j]의 값을 서로 맞바꾼다.
    
     시간복잡도 : Θ(n2)

---
+++ DataStructure/SelectionSort.java
@@ -0,0 +1,27 @@
+class SelectionSort {
+	
+	 public static void main(String args[]) {
+		int[] A = {9,1,6,8,4,3,2,0};
+		getSelectionSort(A);
+	 }
+
+   public static void getSelectionSort(int[] list) {
+        int indexMin, temp;
+
+        for (int i=0; i<list.length-1; i++) {
+            indexMin = i;
+            
+            for (int j=i+1; j<list.length; j++) {
+                if (list[j] < list[indexMin])
+                    indexMin = j;
+            }
+
+            temp = list[indexMin];		//0, 1, 2, 3, 4, 6, 8 
+            list[indexMin] = list[i];	//9, 1, 6, 8, 4, 8
+            list[i] = temp;				//0, 1, 2, 3, 4, 6, 8
+        }
+
+        System.out.println(Arrays.toString(list));
+   }
+   
+}
