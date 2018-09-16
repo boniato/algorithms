c70ffee97e13c74d5f93e101d66081aa090313d4
boniato <teegeegle@gmail.com>
2018-09-16 22:30:46 +0900

    자연수 뒤집어 배열로 만들기
    자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.

--- programmers/Solution17.java
+++ programmers/Solution17.java
@@ -1,5 +1,7 @@
 class Solution {
-  public int[] solution(long n) {
+  
+  //solution1
+  public int[] solution1(long n) {
       int[] answer = {};
       String temp = String.valueOf(n);
       int len = temp.length();
@@ -9,7 +11,32 @@ class Solution {
       for(int i=0; i<len; i++) {
           answer[i] = Integer.parseInt( String.valueOf(temp.charAt(--cnt)) );
       }
-
       return answer;
   }
+  
+  //solution2
+  public int[] solution2(long n) {
+	  int length = Long.toString(n).length();
+      int[] answer = new int[length];
+
+      for (int i = 0; i < length; i++) {
+          answer[i] = (int) (n % 10); //12345%10=5, 1234%10=4, ..
+          n /= 10; // 12345/10=1234, 1234/10=123, ..
+      }
+      return answer;
+  }
+  
+  //solution3
+  public int[] solution3(long n) {
+	  int[] answer = {};
+      String strLong = Long.toString(n);
+
+      answer = new int[strLong.length()];
+      for (int i = 0; i < strLong.length(); i++) {
+    	  String str = ""+strLong.charAt((strLong.length()-i) -1);
+          answer[i] = Integer.parseInt(str);
+      }
+      return answer;
+  }
+  
 }
