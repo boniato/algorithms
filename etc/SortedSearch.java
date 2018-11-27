e51fb89315bb905051bedb8cf22f31046c48ee07
boniato <teegeegle@gmail.com>
2018-11-27 18:02:53 +0900

    [Testdome] SortedSearch

---
+++ etc/SortedSearch.java
@@ -0,0 +1,28 @@
+public class SortedSearch {
+    public static int countNumbers(int[] sortedArray, int lessThan) {
+        
+        int start = 0;
+        int end = sortedArray.length - 1;
+        int mid = 0;
+        
+        while (start <= end) {
+            mid = (start + end) / 2;
+            
+            if (sortedArray[mid] < lessThan) {
+               if (mid < sortedArray.length - 1 && sortedArray[mid + 1] < lessThan) { // check id next value is also valid
+                   start = mid + 1;
+                   continue;
+               } else
+                   return mid + 1;
+           }
+           end = mid - 1;
+        }
+        
+        return 0;
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4));
+    }
+}
