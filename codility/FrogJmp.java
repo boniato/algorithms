3c1597cf93cedd2fd3674c5d975eadb82daf54ec
boniato <teegeegle@gmail.com>
2018-12-06 04:44:39 +0900

    FrogJmp
    https://app.codility.com/programmers/lessons/3-time_complexity/frog_jmp/

--- codility/FrogJmp.java
+++ codility/FrogJmp.java
@@ -1,7 +1,11 @@
 class Solution {
-    public int solution(int X, int Y, int D) {
+    public int solution1(int X, int Y, int D) {
         // write your code in Java SE 8
         int distance = Y - X; //Becauase it starts at X position..
         return distance % D == 0 ? distance/D : distance/D + 1;
+    }
+    
+    public int solution2(int X, int Y, int D) {
+        return (int) Math.ceil((Y - X) / (double) D);
     }
 }
