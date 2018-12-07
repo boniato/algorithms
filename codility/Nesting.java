0eb241d87800a2518f8f06ee84876921be6b14cf
boniato <teegeegle@gmail.com>
2018-12-08 00:59:09 +0900

    Lesson7-1 (Stacks and Queues ) two cases, 100%
    my answer URL
    solution1 : https://app.codility.com/demo/results/trainingPWC93R-SRE/
    solution2 : https://app.codility.com/demo/results/trainingDDRQMM-FAG/
    
    In the solution2, if there are no "que.size() != 2", the solution returned a wrong answer (got 1 expected 0)
    For example, for the input '()(()()(((()())(()()))' the solution returned a wrong answer (got 1 expected 0).

--- codility/Nesting.java
+++ codility/Nesting.java
@@ -2,12 +2,14 @@
  * Complexity:
  * expected worst-case time complexity is O(N);
  * expected worst-case space complexity is O(1) (not counting the storage required for input arguments).
- *
- * my answer URL : https://app.codility.com/demo/results/trainingPWC93R-SRE/
  */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Nesting {
-    public int solution(String S) {
+    
+    public int solution1(String S) {
         //if (S.isEmpty()) return 1;  //The result of this algorithm is the same, whether there is this clause or not.
         
         int counter = 0;
@@ -22,4 +24,24 @@ class Nesting {
 
         return counter == 0 ? 1 : 0;
     }
+    
+    
+    public int solution2(String S) {
+		Queue<Character> que = new LinkedList<>();
+
+		for (int i = 0; i < S.length(); i++) {
+			if (S.charAt(i) == '(') {
+				que.offer('(');
+			} else { // character is ')', if que is empty, S has more ')' than '('
+				if (que.isEmpty())
+					return 0;
+				
+				que.poll();
+			}
+		}
+		
+        //if there are no "que.size() != 2", the solution returned a wrong answer (got 1 expected 0)
+		return que.size() != 2 && que.size() % 2 == 0 ? 1 : 0;
+	}
+    
 }
