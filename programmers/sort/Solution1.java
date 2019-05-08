a22f3562214a3edfe3857ad0ccf42a9edcbada81
boniato <teegeegle@gmail.com>
2019-05-09 00:31:27 +0900

    K번째수

--- programmers/sort/Solution1.java
+++ programmers/sort/Solution1.java
@@ -6,24 +6,15 @@
 import java.util.Arrays;
 
 class Solution1 {
-    public static int[] solution(int[] array, int[][] commands) {
-		int[] answer = new int[commands.length];
-		
-		for(int i=0; i<commands.length; i++)
-	    {
-	        int start = commands[i][0];
-	        int end = commands[i][1];
-	        int k = commands[i][2];
-	        int[] tmp = new int[end - start + 1];
-	        int a = 0;
-	        
-	        for(int j = start-1; j<end; j++)
-	            tmp[a++] = array[j];
-	        
-	        Arrays.sort(tmp);
-	        answer[i] = tmp[k-1];
-	    }
-		
-		return answer;
-	}
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
+        return answer;
+    }
 }
