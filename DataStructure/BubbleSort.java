1854e0d21214d6caf848334486551bd2537ee8fe
boniato <teegeegle@gmail.com>
2019-09-29 02:39:44 +0900

    Update BubbleSort.java

--- DataStructure/BubbleSort.java
+++ DataStructure/BubbleSort.java
@@ -2,29 +2,27 @@ import java.util.Arrays;
 
 class BubbleSort {
 	
-	public static void main(String args[]) {
-		int[] A = {31, 25, 12, 22, 11};
-		//int[] A = {1,2,3};
-		//int[] A = {2,1,1,2,3,1};
-		//String S = "(()(())())";
-		//String S = "((())";
-		//System.out.println("result: " + insertionSort(S));
-		//insertionSort(A);
-		bubbleSort(A);
-	}
+  public static void main(String args[]) {
+	int[] A = {31, 25, 12, 22, 11};
+	//int[] A = {1,2,3};
+	//int[] A = {2,1,1,2,3,1};
+	//String S = "(()(())())";
+	//String S = "((())";
+	bubbleSort(A);
+  }
 
   public static void getBubbleSort(int[] arr) {
       int temp = 0;
 
       for(int i=0; i < arr.length; i++) {
-      for(int j=1 ; j < arr.length-i; j++) {
-        if(arr[j] < arr[j-1]) {
-          temp = arr[j-1];
-          arr[j-1] = arr[j];
-          arr[j] = temp;
-        }
+         for(int j=1 ; j < arr.length-i; j++) {
+              if(arr[j] < arr[j-1]) {
+                temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+              }
+          }
       }
-    }
-    System.out.println(Arrays.toString(arr));
-  }
+     System.out.println(Arrays.toString(arr));
+   }
 }
