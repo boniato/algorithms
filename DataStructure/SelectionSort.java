79af35201f60dc034711449607851fc701913e7d
boniato <teegeegle@gmail.com>
2019-09-29 03:01:49 +0900

    Update SelectionSort.java

--- DataStructure/SelectionSort.java
+++ DataStructure/SelectionSort.java
@@ -16,9 +16,9 @@ class SelectionSort {
                     indexMin = j;
             }
 
-            temp = list[indexMin];		//0, 1, 2, 3, 4, 6, 8 
+            temp = list[indexMin];	//0, 1, 2, 3, 4, 6, 8 
             list[indexMin] = list[i];	//9, 1, 6, 8, 4, 8
-            list[i] = temp;				//0, 1, 2, 3, 4, 6, 8
+            list[i] = temp;		//0, 1, 2, 3, 4, 6, 8
         }
 
         System.out.println(Arrays.toString(list));
