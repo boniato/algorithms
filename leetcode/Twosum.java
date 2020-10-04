b13a32c83bd79a7ec900cb8d860d9a7d3ee469b8
boniato <teegeegle@gmail.com>
2020-10-04 19:13:17 +0900

    Two solutions
    Two solutions

--- leetcode/Twosum.java
+++ leetcode/Twosum.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Twosum {
 
@@ -9,9 +10,13 @@ public class Twosum {
 
         int res[] = twoSum(nums, target);
         System.out.println(Arrays.toString(res));
-
     }
 
+    /* soultion1
+     * time complexity : O(N)
+     * Runtime: 1 ms
+     * Memory Usage: 38.7 MB
+     */
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         Integer res;
@@ -27,6 +32,27 @@ public class Twosum {
         }
 
         return new int[]{1,2};
+    }
+    
+     /* soultion2
+     * time complexity : O(N)
+     * Runtime: 1 ms
+     * Memory Usage: 38.9 MB
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            
+            if (map.containsKey(temp)) {
+                return new int[] { map.get(temp), i} ;
+            }
+            
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
     }
     
 }
