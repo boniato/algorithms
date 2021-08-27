775f3be0511581e8d42b793a520f12e15078eade
Lee <teegeegle@gmail.com>
2021-08-28 02:40:06 +0900

    Update TargetNumber.java

--- programmers/BFS/TargetNumber.java
+++ programmers/BFS/TargetNumber.java
@@ -28,4 +28,21 @@ public class TargetNumber {
         result += bfs(numbers, target, sum-numbers[i], i+1);
         return result;
     }
+    
+    // best solution
+    public int bestSolution(int[] numbers, int target) {
+        int answer = 0;
+        answer = dfs(numbers, 0, 0, target);
+        return answer;
+    }
+    
+    int dfs(int[] numbers, int n, int sum, int target) {
+        if(n == numbers.length) {
+            if(sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        return dfs(numbers, n + 1, sum + numbers[n], target) + dfs(numbers, n + 1, sum - numbers[n], target);
+    }
 }
