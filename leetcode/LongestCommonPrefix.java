7e1dd406f1238ecf145a12076bf2698028b2fe5f
Lee <teegeegle@gmail.com>
2022-01-09 22:13:06 +0900

    Create LongestCommonPrefix.java

---
+++ leetcode/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/longest-common-prefix
+ * Time Complexity: O(N) where N is the size of the array strs[]
+**/
+
+public class LongestCommonPrefix {
+    public static void main (String args[]) {
+        String strs[] = {"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+}
