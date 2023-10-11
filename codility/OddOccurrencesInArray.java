a9f45f80fa83063e757e545208b124a6879ad7f1
Lee <teegeegle@gmail.com>
2023-10-11 15:25:40 +0900

    Update OddOccurrencesInArray.java

--- codility/OddOccurrencesInArray.java
+++ codility/OddOccurrencesInArray.java
@@ -1,3 +1,12 @@
+/**
+ * Given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+ * For example, given array A such that:
+ *   A[0] = 9  A[1] = 3  A[2] = 9
+ *   A[3] = 3  A[4] = 9  A[5] = 7
+ *   A[6] = 9
+ * the function should return 7
+**/
+
 public int solution(int[] A) {
       Set valSet = new HashSet<>(); //Set: There is no sequences(order) and no duplicate values
 
