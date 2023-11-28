98ad22f6d16096867d0743600c11b48f358c19ec
Lee <teegeegle@gmail.com>
2023-11-29 00:31:12 +0900

    Create TargetNumber.java

---
+++ programmers/DFS/TargetNumber.java
@@ -0,0 +1,27 @@
+/**
+* https://school.programmers.co.kr/learn/courses/30/lessons/43165
+* 문제 설명
+* n개의 음이 아닌 정수들이 있습니다. 이 정수들을 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+* 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+**/
+
+class TargetNumber {
+    int answer = 0;
+
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+
+    private void dfs(int sum, int idx, int[] numbers, int target) {
+        if (idx == numbers.length && sum == target) {
+            answer++;
+            return;
+        }
+        if (idx >= numbers.length) {
+            return;
+        }
+        dfs(sum + numbers[idx], idx + 1, numbers, target);
+        dfs(sum - numbers[idx], idx + 1, numbers, target);
+    }
+}
